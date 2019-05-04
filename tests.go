a6a04b6130e7b9d0f1652b052d98e8e14a877f73
James <jismith1989@googlemail.com>
2019-05-04 19:21:21 +0100

    Remove double linebreak

--- tests.go
+++ tests.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func Message(expectedOutput, actualOutput interface{}) (msg string, fail bool) {
 	if actualOutput != expectedOutput {
-		msg = fmt.Sprintf("Test failed:\n\nEXPECTED:\n\t%v\nBUT GOT:\n\t%v", expectedOutput, actualOutput)
+		msg = fmt.Sprintf("Test failed:\nEXPECTED:\n\t%v\nBUT GOT:\n\t%v", expectedOutput, actualOutput)
 		fail = true
 	}
 
